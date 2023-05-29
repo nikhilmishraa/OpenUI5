2fffcc4209823835f767df45148f66ec9c853ad5
Nikhil Mishra <nikhilmishra262@gmail.com>
2023-05-29 23:57:59 +0530

    Step 4: XML Views

--- webapp/index.js
+++ webapp/index.js
@@ -1,9 +1,12 @@
 sap.ui.define([
-    "sap/m/Text"
-], function(Text) {
-    'use strict';
-    
-    new Text({
-        text: "Hello World"
-    }).placeAt("content");
+	"sap/ui/core/mvc/XMLView"
+], function (XMLView) {
+	"use strict";
+
+	XMLView.create({
+		viewName: "sap.ui.demo.walkthrough.view.App"
+	}).then(function (oView) {
+		oView.placeAt("content");
+	});
+
 });
